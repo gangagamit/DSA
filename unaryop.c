e62119622d234012274e99b118ed2426daab642a
gangagamit <gangagamit350@gmail.com>
2023-07-20 10:05:45 +0530

    20-07-2023

---
+++ unaryop.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+   int a=9;
+   //printf("a: %d,b: %d",a,a++);
+   //printf("a: %d,b: %d",a++,a++);
+   //printf("a: %d, b: %d",a,++a);
+   //printf("a: %d,b: %d",a++,++a); 
+   //printf("a: %d, b: %d,c: %d",a,a++,++a);
+   //printf("a: %d,b: %d,c: %d",a++,a++,++a);
+   //printf("a: %d,b: %d,c: %d",++a,a++,++a);
+   //printf("a: %d,b: %d,c: %d",++a,++a,a++);
+   //printf("a; %d,b: %d,c: %d",a++,++a,++a);
+   //printf("a: %d,b: %d,c: %d",++a,a,a++);
+   //printf("a: %d,b: %d,c: %d",a,++a,++a);
+   //printf("a: %d,b: %d,c: %d",++a,a,a++);
+   // printf("a: %d,b: %d,c: %d",a,a,++a);
+   //printf("a: %d, b: %d, c: %d",++a,a,a);
+   // printf("a: %d,b: %d,c: %d",a++,++a,a);
+   //printf("a: %d,b: %d,c: %d",++a,a,a);
+   //printf("a: %d,b: %d, c: %d",a,++a,++a);
+  printf("a: %d,b: %d, c: %d",++a,a,a++);
+}
